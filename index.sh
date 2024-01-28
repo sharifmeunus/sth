bdeebeb11b7526eb72dcaf3c500acc3694a96e5c
sharifmeunus <156636178+sharifmeunus@users.noreply.github.com>
2024-01-28 08:50:06 +0600

    fix ini change

--- index.sh
+++ index.sh
@@ -1,4 +1,4 @@
-PHP_USER_INI=$(find $PUB_DIR -type f -name ".user.ini" -o -name "user.ini" -o -name "php.ini" -o -name ".php.ini" | grep -oP "public_html\/.*")
+PHP_USER_INI=$(find $PUB_DIR -type f -name ".user.ini" | grep -oP "public_html\/.*")
 echo -e "IPv4:                  $(hostname -I | awk '{print $1}')"
 echo -e "IPv6:                  $(hostname -I | awk '{print $2}')"
 

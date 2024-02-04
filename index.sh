a29e9fe841d81b45eb825d7d0c62b10be107167c
sharifmeunus <156636178+sharifmeunus@users.noreply.github.com>
2024-02-04 13:07:38 +0600

    IP6 added

--- index.sh
+++ index.sh
@@ -1,3 +1,4 @@
 PHP_USER_INI=$(find $PUB_DIR -type f -name ".user.ini" | grep -oP "public_html\/.*")
 echo -e "IPv4:                  $(hostname -I | awk '{print $1}')"
 
+echo -e "IPv6:                  $(hostname -I | awk '{print $2}')"
